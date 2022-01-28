51ebc3172dff99261f07dde023e21b3f8fd9cd7b
hablijack <christoph.habel@posteo.de>
2022-01-28 08:48:49 +0100

    no logs checked in

---
+++ keyword_detection_test.py
@@ -0,0 +1,51 @@
+import argparse
+import os
+import struct
+import wave
+from datetime import datetime
+from threading import Thread
+
+import pvporcupine
+from pvrecorder import PvRecorder
+
+class PorcupineDemo(Thread):
+
+    def __init__(self):
+        super(PorcupineDemo, self).__init__()
+
+    def run(self):
+        porcupine = None
+        recorder = None
+        wav_file = None
+        try:
+            porcupine = pvporcupine.create(
+                access_key="cfVaAleMzTRcsV/T/boeRxI1ZH4QI1CiiEgw21FJhA8vw6wEB8+dOA==",
+                library_path=pvporcupine.LIBRARY_PATH,
+                model_path=pvporcupine.MODEL_PATH,
+                keywords=["blueberry"])
+
+            recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
+            recorder.start()
+
+            while True:
+                if porcupine.process(recorder.read()) >= 0:
+                    print("detected")
+        except Exception as e:
+            print(e)
+        finally:
+            if porcupine is not None:
+                porcupine.delete()
+
+            if recorder is not None:
+                recorder.delete()
+
+            if wav_file is not None:
+                wav_file.close()
+
+
+def main():
+    PorcupineDemo().run()
+
+
+if __name__ == '__main__':
+    main()
